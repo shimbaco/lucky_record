1f438d32e3fca4e333b411c4e8f6a467a4ed859e
Paul Smith <paulcsmith0218@gmail.com>
2017-12-01 16:14:27 -0500

    Add failing spec

---
+++ spec/preloading_spec.cr
@@ -0,0 +1,23 @@
+require "./spec_helper"
+
+describe "Preloading" do
+  it "preloads_queries" do
+    begin
+      lazy_load(enabled: false)
+      post = PostBox.save
+      comment = CommentBox.new.post_id(post.id).save!
+
+      posts = Post::BaseQuery.new.preload_comments
+
+      posts.comments.should eq([comment])
+    ensure
+      lazy_load(enabled: true)
+    end
+  end
+end
+
+private def lazy_load(enabled)
+  LuckyRecord::Model.configure do
+    settings.lazy_load_enabled = enabled
+  end
+end
