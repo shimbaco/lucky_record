854b0fa3509c74e4a87db0d8e667107fb4cd0660
Paul Smith <paulcsmith0218@gmail.com>
2017-12-01 22:09:16 -0500

    Get ready for next steps

--- spec/preloading_spec.cr
+++ spec/preloading_spec.cr
@@ -2,36 +2,46 @@ require "./spec_helper"
 
 describe "Preloading" do
   it "can disable lazy loading" do
-    begin
-      lazy_load(enabled: false)
-
+    with_lazy_load(enabled: false) do
       posts = Post::BaseQuery.new
 
       expect_raises LuckyRecord::LazyLoadError do
         posts.first.comments
       end
-    ensure
-      lazy_load(enabled: true)
     end
   end
 
   it "preloads_queries" do
-    begin
-      lazy_load(enabled: false)
+    with_lazy_load(enabled: false) do
       post = PostBox.save
       comment = CommentBox.new.post_id(post.id).save!
 
       posts = Post::BaseQuery.new.preload_comments
 
       posts.results.first.comments.should eq([comment])
-    ensure
-      lazy_load(enabled: true)
     end
+  end
+
+  it "works with nested preloads" do
+  end
+
+  it "uses an empty array if there are no associated records" do
+  end
+
+  it "works with belongs_to" do
   end
 end
 
-private def lazy_load(enabled)
-  LuckyRecord::Model.configure do
-    settings.lazy_load_enabled = enabled
+private def with_lazy_load(enabled)
+  begin
+    LuckyRecord::Model.configure do
+      settings.lazy_load_enabled = enabled
+    end
+
+    yield
+  ensure
+    LuckyRecord::Model.configure do
+      settings.lazy_load_enabled = true
+    end
   end
 end
